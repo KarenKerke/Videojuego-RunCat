166809f5feca82781d6f0a264dc51bdadcd428fa
karenkerke <karenkerke@gmail.com>
2025-11-30 18:11:21 -0300

    Agregar código del juego

---
+++ ObjetoDeJuego.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run_Cat
+{
+    internal class ObjetoDeJuego
+    {
+        //Atributos
+        public int posicion;
+
+        //Metodos 
+        public void Movimiento()
+        {
+            posicion
+        }
+    }
+}
